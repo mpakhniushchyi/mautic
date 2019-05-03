2989884712a5c79226d0c887b3d1738ea99c420e
MaksymP <mpakhniushchyi@alphawebgroup.com>
2019-05-03 10:30:47 +0300

    Issue #22063: Add a target attribute to the vml button

--- app/bundles/CoreBundle/Views/Slots/vmlbutton.html.php
+++ app/bundles/CoreBundle/Views/Slots/vmlbutton.html.php
@@ -13,13 +13,14 @@
     <!--[if mso]>
     <v:roundrect
         class="vml-button-shape"
+        target="_blank"
         xmlns:v="urn:schemas-microsoft-com:vml"
         xmlns:w="urn:schemas-microsoft-com:office:word" href="#"
         style="height:36px;v-text-anchor:middle;width:200px;"
         arcsize="9%" strokecolor="#4e5d9d" fillcolor="#4e5d9d">
         <w:anchorlock/>
-        <center class="vml-button-label" style="color:#ffffff;font-family:Arial,sans-serif;font-size:13px;">I want this!</center>
+        <center class="vml-button-label" style="color:#ffffff;font-family:Arial,sans-serif;font-size:16px;">I want this!</center>
     </v:roundrect>
     <![endif]-->
-    <a href="#" class="vml-button-link" style="background-color:#4e5d9d;border:1px solid #4e5d9d;border-radius:3px;color:#ffffff;display:inline-block;font-family:Arial,sans-serif;font-size:13px;line-height:36px;text-align:center;text-decoration:none;width:200px;-webkit-text-size-adjust:none;mso-hide:all;">I want this!</a>
+    <a href="#" target="_blank" class="vml-button-link" style="background-color:#4e5d9d;border:1px solid #4e5d9d;border-radius:3px;color:#ffffff;display:inline-block;font-family:Arial,sans-serif;font-size:16px;line-height:36px;text-align:center;text-decoration:none;width:200px;-webkit-text-size-adjust:none;mso-hide:all;">I want this!</a>
 </div>
